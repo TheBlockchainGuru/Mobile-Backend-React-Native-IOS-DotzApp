341b508b051a747a7380f7f4bb3c8c76a9e0b5db
timoflask <88914566+timoflask@users.noreply.github.com>
2021-08-25 04:30:24 +0300

    small change in mediacomment model

--- app/Models/MediaCommentModel.php
+++ app/Models/MediaCommentModel.php
@@ -7,5 +7,5 @@ class MediaCommentModel extends Model
     protected $table = 'media_comments';
     protected $primaryKey = 'id';
 
-    protected $allowedFields = ['app_user_id', 'user_media_id', 'comment'];
+    protected $allowedFields = ['app_user_id', 'user_media_id', 'comment', 'parent_id'];
 }
